95a0789e4749f291ce814167960f43ac5dd7b6b4
Frity2025 <frity362@gmail.com>
2023-10-18 01:58:34 -0700

    Task

---
+++ _shellLoop.c
@@ -0,0 +1,26 @@
+#include "simpleshell.h"
+
+/**
+* _cmnd - it prepare the command line
+*
+* Return: the command line
+*/
+
+char *_cmnd(void)
+{
+char *input = NULL;
+size_t size = 0;
+
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "$ ", 2);
+
+if (getline(&input, &size, stdin) == -1)
+{
+free(input);
+return (NULL);
+}
+
+return (input);
+}
+
+
---
+++ free_ptr.c
@@ -0,0 +1,20 @@
+
+#include "simpleshell.h"
+/**
+ * ptr_free -free two dimensional array
+ * @argv: free two dimensional array.
+ * @h: height of array
+ *
+ * Return: NULL
+ */
+void ptr_free(char **argv, int h)
+{
+	int a = 0;
+
+	while (a < h)
+	{
+		free(argv[a]);
+		a++;
+	}
+	free(argv);
+}
